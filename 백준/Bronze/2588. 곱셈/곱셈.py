73c8e564466c4eb7147a94ffbfbb401c512a7677
Minah Han <mi.in.ah0403@gmail.com>
2023-08-16 22:39:18 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,8 @@
+A = int(input())
+B = input()
+result_3 = A*int(B[2])
+result_4 = A*int(B[1])
+result_5 = A*int(B[0])
+result_6 = A*int(B)
+                
+print(result_3, result_4, result_5, result_6, sep="\n")
